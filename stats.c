d3431e2989b7e7e9a540c370dc07017cef0f3801
PabloSaro <abeoxsaro@gmail.com>
2023-02-08 12:59:24 +0100

    third commit, stats.c moddified

--- stats.c
+++ stats.c
@@ -84,7 +84,7 @@ void sort_array(unsigned char *array, unsigned int n){
    int i,j;
    for(i = 0;i < n-1;i++) {
       for(j = 0;j < n-i-1;j++) {
-         if(array[j] > array[j+1])
+         if(array[j] < array[j+1])
             swap(&array[j],&array[j+1]);
       }
    }
